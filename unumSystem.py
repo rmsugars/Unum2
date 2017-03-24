8f7fe04667cb51d2f58beff0e0d8c0fd8ae6afb6
rmsugars <reuben.sugars@gmail.com>
2017-03-24 14:56:19 +1030

    generates exact numbers

--- unumSystem.py
+++ unumSystem.py
@@ -1,4 +1,23 @@
 class unumSys:
+	exacts = {}
 	def __init__(self, lattice):
 		self.lattice = lattice
-		
+		self.generateNums()
+
+	def generateNums(self):
+		latLen = len(self.lattice)
+		for i,num in enumerate(self.lattice):
+			self.exacts[latLen-i] =1/num
+			self.exacts[latLen+i] = num
+			self.exacts[3*latLen-i] = -num
+			self.exacts[3*latLen+i] = -1/num
+
+	def outputExacts(self):
+		tries = max(self.exacts)+1
+		numbits = 7
+		for i in range(tries):
+			try:
+				temp = self.exacts[i]
+			except:
+				temp = "none"
+			print("{0:0={1}b} => {2}".format(i,numbits,temp))
